f3d83e63fe477ee08b5a26318cb21ab78e3f2ecd
tournevis <arthur.ro92@gmail.com>
2014-12-18 19:27:52 +0100

    Fourth push

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,35 +1,36 @@
 'use strict';
-console.log('Loaded');
-
-
 var gareX = 1030;
 var firstBridge = 2030;
 var dataMountain = 3100;
 var secondBridge = 4580;
-var dataMeteo = 7200;
+var dataMeteo = 7250;
 var thridBridge = 8740;
 var tunnelExit = 11900;
+var insideTrain = 14000;
 var trainX;
 
 var cloud = '../images/nuages.png';
 var star = '../images/etoile.png';
-var dataMountain = $( ".dataMountain" );
-var adaptator = $(".adaptator");
-
-var firstDial = $('<p class="appendText"> Les passagers Embarquent</p>');
+var dataMountain = $( '.dataMountain' );
+var adaptator = $('.adaptator');
+var firstDial = $('<p class="appendTextTrain">Let\'s go !</p>');
 var dataPass;
-var data = $('<p class="appendText"> 9 : 30 </p>');
+var colorInd = 0;
+
+var score = 0;
+var scoreCount = false;
+
 
 dataMountain.height(0);
 
 
-  /* Parallax.js Init */
+/* Parallax.js Init */
 
 var scene = document.getElementById('middleGround');
 var parallax = new Parallax(scene);
 //parallax.origin(0.0, 0.0);
 
-  /* Canvas Part and Particule part */
+/* Canvas Part and Particule part */
 
 var canvas = document.createElement('canvas');
 var ctx = canvas.getContext('2d');
@@ -55,23 +56,19 @@ function Particle(imagesrc){
   //context.beginPath();
   //ctx.drawImage(etoileImg, x,y);
 }
-Particle.prototype.render = function(){
 
-}
-
-
-dataPass = $.getJSON('http://anarchy.rayanmestiri.com/ecs-rushhour/morning/SANN');
- console.log(dataPass);
+//dataPass = $.getJSON('http://anarchy.rayanmestiri.com/ecs-rushhour/morning/SANN');
+// console.log(dataPass);
 
 /* JQuery Animate part  */
 
 $( window ).load(function() {
   adaptator.height((window.innerHeight - 700)/2) ;
   var fromLeft = $(window).scrollLeft();
-  $( "#train" ).animate({
+  $( '#train' ).animate({
     left: window.innerWidth/4 + fromLeft
     }, 1000); 
-  $( ".nav" ).animate({
+  $( '.nav' ).animate({
         left: window.innerWidth/2 +fromLeft -100
       }, 0);   
   for (var i = 0; i <= 100; i++) {
@@ -87,74 +84,103 @@ $( window ).load(function() {
 $( window ).resize(function() {
   adaptator.height((window.innerHeight - 700)/2) ;
   if(adaptator.height() < 30){
-    $( ".nav" ).css({
-      position: absolute
+    $( '.nav' ).css({
+      'position': 'absolute'
     });
   }
 });
+/** INVERT SCROLLING  **/
+$(function() {
+   $('body').mousewheel(function(event, delta) {
+      this.scrollLeft -= (delta * 0.8);
+      event.preventDefault();
+   });
+});
 
 
+
+$('.nav1').click(function(){
+  colorInd = 1;
+  $('body').animate({
+    scrollLeft : gareX
+  },500);
+});
+$('.nav2').click(function(){
+   colorInd = 2;
+  $('body').animate({
+    scrollLeft : 3000
+  },500);
+});
+$('.nav3').click(function(){
+   colorInd = 3;
+  $('body').animate({
+    scrollLeft : dataMeteo
+  },500);
+});
+$('.nav4').click(function(){
+   colorInd = 4;
+  $('body').animate({
+    scrollLeft : 14000
+  },500);
+});
 $(document).ready(function(){
 
 });
 
-$(".nav1").click(function(){
-  $("body").animate({
-    scrollLeft : gareX
-  },500);
+$('.btn').click(function(){
+  $('.btn').removeClass('selected');
+  $(this).addClass('selected');
 });
-$(".nav2").click(function(){
-  $("body").animate({
-    scrollLeft : 3000
-  },500);
-});
-$(".nav3").click(function(){
-  $("body").animate({
-    scrollLeft : dataMeteo
-  },500);
-});
-$(".nav4").click(function(){
-  $("body").animate({
-    scrollLeft : 14000
-  },500);
-});
-
 
 $(window).scroll(function(){
     // Get container scroll position
   var fromLeft = $(this).scrollLeft();
-  trainX = $( "#train" ).position().left;
-
-  $( "#train" ).animate({
+  trainX = $( '#train' ).position().left;
+  console.log(trainX);
+  $( '#train' ).animate({
       left: window.innerWidth/4 +fromLeft
   	}, 0);
-    $( ".nav" ).animate({
+    $( '.nav' ).animate({
         left: window.innerWidth/2 +fromLeft -100
       }, 0);    // console.log(fromLeft);	
-     //console.log($( ".end" ).position().left);
+     //console.log($( '.end' ).position().left);
   if( trainX > gareX && trainX < gareX+1000){
     firstDial.fadeIn().appendTo($('#train'));
   }else{
-    firstDial.fadeOut("slow").remove() ;
+    firstDial.fadeOut('slow').remove() ;
   } 
   if(trainX > 3000){
-    $( ".data1" ).animate(
-      { height: '550' }, 500, function(){
-        data.fadeIn().appendTo($(this));
-        $( ".data2" ).animate(
-          { height: '450' }, 500, function(){
-            $( ".data3" ).animate(
-            { height: '400' }, 500, function(){
-              $( ".data4" ).animate(
-            { height: '500' }, 500, function(){
-          });
-        });
-      });
-    });
+    animateMountain();
   }  
+  if(trainX > dataMeteo){
+    animateMeteo();
+  }
+  if(trainX > insideTrain){
+      seatCount();
+  }
 });
 
+function animateMountain(){
+  var tl = new TimelineLite();
+  tl.to($( '.data1' ),0.5,{height:'550px'},'-=0.1');
+  tl.to($( '.data2' ),0.5,{height:'450px'},'-=0.1');
+  tl.to($( '.data3' ),0.5,{height:'400px'},'-=0.1');
+  tl.to($( '.data4' ),0.5,{height:'500px'},'-=0.1');
+}
+function animateMeteo(){
+  var tl = new TimelineLite();
+  tl.to($( '#m1' ),0.5,{opacity:1},'+=0.1');
+  tl.to($( '#m2' ),0.5,{opacity:1},'+=0.1');
+  tl.to($( '#m3' ),0.5,{opacity:1},'+=0.1');
+  tl.to($( '#m4' ),0.5,{opacity:1},'+=0.1');
+}
+function seatCount(){
 
-
-
-//console.log(document.getElementsByTagName("body").width);
+  $( "#seat" ).text(score);
+  if(score < 30){
+    score += 2 ;
+  }else{
+   score = 30;
+ }
+  
+}
