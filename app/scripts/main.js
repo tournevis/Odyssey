f40a53e85836872a2ff964136283d379f9266c81
Tournevis <arthur.ro92@gmail.com>
2014-12-18 11:09:52 +0100

    third push

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,11 @@
 console.log('Loaded');
 
 
-var gareX = 1330;
+var gareX = 1030;
 var firstBridge = 2030;
-var dataMountain = 3000;  
+var dataMountain = 3100;
 var secondBridge = 4580;
-var dataMeteo = 7330;
+var dataMeteo = 7200;
 var thridBridge = 8740;
 var tunnelExit = 11900;
 var trainX;
@@ -14,18 +14,20 @@ var trainX;
 var cloud = '../images/nuages.png';
 var star = '../images/etoile.png';
 var dataMountain = $( ".dataMountain" );
+var adaptator = $(".adaptator");
 
 var firstDial = $('<p class="appendText"> Les passagers Embarquent</p>');
 var dataPass;
-var data = $('<p class="appendText"> Data</p>');
+var data = $('<p class="appendText"> 9 : 30 </p>');
 
 dataMountain.height(0);
 
 
-  /* Parallax.js Initi */
+  /* Parallax.js Init */
+
 var scene = document.getElementById('middleGround');
 var parallax = new Parallax(scene);
-parallax.origin(0.0, 1.0);
+//parallax.origin(0.0, 0.0);
 
   /* Canvas Part and Particule part */
 
@@ -59,59 +61,99 @@ Particle.prototype.render = function(){
 
 
 dataPass = $.getJSON('http://anarchy.rayanmestiri.com/ecs-rushhour/morning/SANN');
-  console.log(dataPass);
+ console.log(dataPass);
+
 /* JQuery Animate part  */
+
 $( window ).load(function() {
-  
+  adaptator.height((window.innerHeight - 700)/2) ;
   var fromLeft = $(window).scrollLeft();
   $( "#train" ).animate({
     left: window.innerWidth/4 + fromLeft
-    }, 1000);  
+    }, 1000); 
+  $( ".nav" ).animate({
+        left: window.innerWidth/2 +fromLeft -100
+      }, 0);   
   for (var i = 0; i <= 100; i++) {
-
       Particle(star);
   };
   for (var i = 0; i <= 30; i++) {
       Particle(cloud);
   };
 });
-$(document).ready(function(){
-  $(window).scroll(function(){ 
-      // Get container scroll position
-     var fromLeft = $(this).scrollLeft();
-     trainX = $( "#train" ).position().left;
 
-     $( "#train" ).animate({
-        left: window.innerWidth/4 +fromLeft
-    	}, 0);
-    // console.log(fromLeft);	
+/***** RESPONSIVE PART  ****/
+
+$( window ).resize(function() {
+  adaptator.height((window.innerHeight - 700)/2) ;
+  if(adaptator.height() < 30){
+    $( ".nav" ).css({
+      position: absolute
+    });
+  }
+});
+
+
+$(document).ready(function(){
+
+});
+
+$(".nav1").click(function(){
+  $("body").animate({
+    scrollLeft : gareX
+  },500);
+});
+$(".nav2").click(function(){
+  $("body").animate({
+    scrollLeft : 3000
+  },500);
+});
+$(".nav3").click(function(){
+  $("body").animate({
+    scrollLeft : dataMeteo
+  },500);
+});
+$(".nav4").click(function(){
+  $("body").animate({
+    scrollLeft : 14000
+  },500);
+});
+
+
+$(window).scroll(function(){
+    // Get container scroll position
+  var fromLeft = $(this).scrollLeft();
+  trainX = $( "#train" ).position().left;
+
+  $( "#train" ).animate({
+      left: window.innerWidth/4 +fromLeft
+  	}, 0);
+    $( ".nav" ).animate({
+        left: window.innerWidth/2 +fromLeft -100
+      }, 0);    // console.log(fromLeft);	
      //console.log($( ".end" ).position().left);
-     if( trainX > gareX && trainX < gareX+1000){
-        firstDial.fadeIn().appendTo($('#train'));
-      }else{
-        firstDial.fadeOut("slow").remove() ;
-      } 
-      if(trainX > 3000){
-        $( ".data1" ).animate(
-          { height: '550' }, 500, function(){
-            data.fadeIn().appendTo($(this));
-            $( ".data2" ).animate(
-            { height: '300' }, 300, function(){
-             $( ".data3" ).animate(
-              { height: '500' }, 500, function(){
-        
-            });
+  if( trainX > gareX && trainX < gareX+1000){
+    firstDial.fadeIn().appendTo($('#train'));
+  }else{
+    firstDial.fadeOut("slow").remove() ;
+  } 
+  if(trainX > 3000){
+    $( ".data1" ).animate(
+      { height: '550' }, 500, function(){
+        data.fadeIn().appendTo($(this));
+        $( ".data2" ).animate(
+          { height: '450' }, 500, function(){
+            $( ".data3" ).animate(
+            { height: '400' }, 500, function(){
+              $( ".data4" ).animate(
+            { height: '500' }, 500, function(){
           });
         });
-      }
-     
-        
-      
-
-       
-      
-  });
+      });
+    });
+  }  
 });
+
 
 
 
